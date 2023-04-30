c95f03d33511d126c29779f11efd571520d50ac4
Coral Jimenez <info.coraljimenez@gmail.com>
2023-04-30 15:29:59 +0200

    Añadidos los dos primeros gifs

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function selectA(){
+    let screen = document.getElementById("idScreen");
+    screen.classList.add("selectOn");
+}
+
+function clickbuttonA(){
+    let screen = document.getElementById("idScreen");
+    screen.classList.add("onButtonA");
+}
