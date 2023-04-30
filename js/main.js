e329d46c86d3fe84d356bad35745460b3c3f8529
Coral Jimenez <info.coraljimenez@gmail.com>
2023-04-30 16:32:28 +0200

    Acciones de js acabadas

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ function clickbuttonA(){
     let screen = document.getElementById("idScreen");
     screen.classList.add("onButtonA");
 }
+
+function powerOn(){
+    let screen = document.getElementById("idScreen");
+    let on = document.getElementById("idpowerlight");
+    screen.classList.add("onPower");
+    on.classList.add("powershadow")
+}
